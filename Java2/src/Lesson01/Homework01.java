d6b13112c39d5cf89a2a2700d1625c5f667de3c6
RomanMin <1@ali.m-rv.ru>
2021-09-14 21:13:34 +0300

    Java Core Lesson01

--- Java2/src/Lesson01/Homework01.java
+++ Java2/src/Lesson01/Homework01.java
@@ -1,37 +1,4 @@
 package Lesson01;
 
 public class Homework01 {
-    public static void main(String[] args) {
-
-        Course[] currentCourse = new Course[3];
-        currentCourse[0] = new Course(1000, 1200, 900);
-        currentCourse[1] = new Course(1000, 1200, 750);
-        currentCourse[2] = new Course(1000, 1200, 900);
-
-
-
-        Team[] teamMates = new Team[4];
-        teamMates[0] = new Team("Team1", "Pavel", 1000);
-        teamMates[1] = new Team("Team1", "Oleg", 890);
-        teamMates[2] = new Team("Team1", "Ivan", 1200);
-        teamMates[3] = new Team("Team1", "Petr", 1300);
-
-
-        System.out.println("Участники Соревнований");
-        for (Team teamMate : teamMates) {
-            teamMate.printData();
-        }
-        System.out.println("Победители соревнований");
-        for (Team teamMate : teamMates) {
-            for (Course course : currentCourse) {
-                if (teamMate.curentDistanse >= course.runDistanse)
-                    teamMate.printData();
-
-            }
-        }
-    }
 }
-
-
-
-
