7f720a6e34aa192158d096f5067311ffef211430
RomanMin <1@ali.m-rv.ru>
2021-09-21 01:02:54 +0300

    Java Core Lesson01

--- Java2/src/Lesson01/Homework01.java
+++ Java2/src/Lesson01/Homework01.java
@@ -5,13 +5,13 @@ public class Homework01 {
 
         Course[] currentCourse = new Course[3];
         currentCourse[0] = new Course(1000, 1200, 900);
-        currentCourse[1] = new Course(1000, 1200, 900);
+        currentCourse[1] = new Course(1000, 1200, 750);
         currentCourse[2] = new Course(1000, 1200, 900);
 
 
         Team[] teamMates = new Team[4];
-        teamMates[0] = new Team("Team1", "Pavel", 1010);
-        teamMates[1] = new Team("Team1", "Oleg", 990);
+        teamMates[0] = new Team("Team1", "Pavel", 1000);
+        teamMates[1] = new Team("Team1", "Oleg", 890);
         teamMates[2] = new Team("Team1", "Ivan", 1200);
         teamMates[3] = new Team("Team1", "Petr", 1300);
 
